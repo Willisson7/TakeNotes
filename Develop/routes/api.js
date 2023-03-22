d46c8ffe9fe5473a3adb0bb52517b73048a04780
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-03-22 00:17:39 -0700

    Corrected delete route. Can now successfully save and delete notes.

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const express = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
-express.get('/', (req, res) => {
+let dbData = require('../db/db.json');
+
+
+express.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) =>
         res.json(JSON.parse(data))
     );
@@ -9,7 +13,7 @@ express.get('/', (req, res) => {
 
 express.post('/notes', (req, res) => {
     // Inform the client that their POST request was received
-    res.json(`${req.method} request received to add a note`);
+    // res.json(`${req.method} request received to add a note`);
 
     console.log(req.body);
     const { title, text } = req.body;
@@ -17,19 +21,33 @@ express.post('/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            title_id: uuid(),
+            id: uuid(),
         };
         console.log(newNote);
-        
-        readAndAppend(newNote, './db/db.json');
-        res.json('New note added successfully!');
+        dbData.push(newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(dbData, null, "\t"));
+        res.json('dbData updated successfully!');
     } else {
         res.error('Could not add new note.');
     }
+});
 
-    // Log our request to the terminal
-    console.info(`${req.method} request received to add a note`)
-  });
+//     // Log our request to the terminal
+
+express.delete('/notes/:id', (req, res) => {
+    console.info(`${req.method} request received to delete a note`)
+    readFromFile('./db/db.json').then((data) => {
+        let keepNote = [];
+        let newData = JSON.parse(data);
+        for (var i = 0; i < newData.length; i++) {
+            if (newData[i].id != req.params.id) {
+                keepNote.push(newData[i]);
+            }
+        }
+        fs.writeFileSync('./db/db.json', JSON.stringify(keepNote));
+        res.json('Note succesffully Deleted');
+    })
+});
 
 
 module.exports = express;
--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const path = require('path');
 const {mlog} = require('./middleware/mlog');
 const apiRouter = require('./routes/api');
-const notesRouter = require('./routes/notes');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
 
 // place Json middleware
 app.use(mlog);
@@ -14,31 +12,17 @@ app.use(mlog);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', apiRouter);
-app.use('/', notesRouter);
-
-
 app.use(express.static("public"));
-// GET 
+app.use('/api', apiRouter);
+
 
 app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, './public/index.html'))
 ); 
 
-
-// GET 
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, './public/notes.html'))
 ); 
-
-
-// app.post('api/notes', (req, res) => {
-//     // Inform the client that their POST request was received
-//     res.json(`${req.method} request received to add a note`);
-  
-//     // Log our request to the terminal
-//     console.info(`${req.method} request received to add a note`);
-//   });
 
 
 // Place PORT listen
